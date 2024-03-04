e8b585f7162004784d48c2046970efa119df0b9e
alik-balika <balikaalik@gmail.com>
2024-03-03 21:54:20 -0800

    add grid look to displays inputs and add ability to edit specific grid items

--- src/pages/RoomPage.jsx
+++ src/pages/RoomPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography, TextField } from "@mui/material";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import io from "socket.io-client";
@@ -10,10 +10,52 @@ import "./roomPage.css";
 
 const socket = io.connect("http://localhost:7000");
 
+const samplePlayers = [
+  {
+    input: "shrek",
+    belongsTo: "player1",
+    ownedBy: "player1",
+  },
+  {
+    input: "inception",
+    belongsTo: "player2",
+    ownedBy: "player2",
+  },
+  {
+    input: "interstellar",
+    belongsTo: "player3",
+    ownedBy: "player3",
+  },
+  {
+    input: "top gun",
+    belongsTo: "player4",
+    ownedBy: "player4",
+  },
+];
+
 const RoomPage = () => {
   const { roomName } = useParams();
   const [roomExists, setRoomExists] = useState(true);
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(samplePlayers);
+
+  const [editingIndex, setEditingIndex] = useState(-1);
+  const [editedInput, setEditedInput] = useState("");
+
+  const handleDoubleClick = (index) => {
+    setEditingIndex(index);
+    setEditedInput(players[index].input);
+  };
+
+  const handleInputChange = (e) => {
+    setEditedInput(e.target.value);
+  };
+
+  const handleInputBlur = () => {
+    const updatedPlayers = [...players];
+    updatedPlayers[editingIndex].input = editedInput;
+    setPlayers(updatedPlayers);
+    setEditingIndex(-1);
+  };
 
   useEffect(() => {
     socket.on("playerJoined" + roomName, (data) => {
@@ -34,7 +76,7 @@ const RoomPage = () => {
         if (error.response && error.response.status === 404) {
           setRoomExists(false);
         } else {
-          alert("Error while retrieving room:", error);
+          // alert("Error while retrieving room:", error);
         }
       }
     };
@@ -46,15 +88,50 @@ const RoomPage = () => {
     <Stack direction="column" alignItems="center">
       <Header title={roomName} />
       {roomExists ? (
-        <Box mt={2} width="300px">
-          <Typography variant="body1" align="center" mb={4} className="loading">
-            Waiting for players
-          </Typography>
-          {players.map((player) => (
-            <p key={player}>player</p>
-          ))}
-          <StyledButton text="Start" />
-        </Box>
+        !players.length ? (
+          <Box mt={2} width="300px">
+            <Typography
+              variant="body1"
+              align="center"
+              mb={4}
+              className="loading"
+            >
+              Waiting for players
+            </Typography>
+            <StyledButton text="Start" />
+          </Box>
+        ) : (
+          <Grid container p={3}>
+            {players.map((player, index) => (
+              <Grid key={index} item xs={6}>
+                {editingIndex === index ? (
+                  <TextField
+                    fullWidth
+                    value={editedInput}
+                    onChange={handleInputChange}
+                    onBlur={handleInputBlur}
+                    onClick={(e) => e.target.select()}
+                    autoFocus
+                    sx={{
+                      padding: 0,
+                      "& .MuiInputBase-input": {
+                        padding: "10px",
+                      },
+                    }}
+                  />
+                ) : (
+                  <Typography
+                    sx={{ border: "1px solid black", cursor: "pointer" }}
+                    p={1}
+                    onClick={() => handleDoubleClick(index)}
+                  >
+                    {player.input}
+                  </Typography>
+                )}
+              </Grid>
+            ))}
+          </Grid>
+        )
       ) : (
         <Typography variant="body1" align="center">
           Room <span style={{ fontWeight: "bold" }}>{roomName}</span> does not
